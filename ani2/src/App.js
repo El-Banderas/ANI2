ddb90652ea3693f9ca5e52b17428ac12c9da0fdc
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2023-10-08 18:56:37 +0100

    Add my stuff?

--- ani2/src/App.js
+++ ani2/src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import TechMain from './TechnicianPage/TechnicianMain'
+import NoPage from './NoPage'
+import LandPage from './LandPage/LandPage'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+
+  const tasks = {
+    "Task1": 5,
+    "Task2": 7,
+    "Task3": 3,
+    "Task4": 9,
+    "Task5": 5,
+    "Task6": 5,
+    "Task7": 5,
+    "Task8": 1,
+    "Task9": 5,
+    "Task10": 10,
+    "Task11": 5,
+  }
+
+  const technicians = {
+    "Tec1": ["Task1", "Task2", "Task3", "Task4"],
+    "Tec2": ["Task5", "Task6", "Task7", "Task8"],
+    "Tec3": ["Task9", "Task10", "Task11"]
+  }
+
+  const getInput = () => {
+    return { "tasks": tasks, "technicians": technicians }
+  }
+
+  const getTasksTech = (name) => {
+    const input = getInput()
+    const tasksTech = input["technicians"][name]
+    const res = []
+    for (let task of tasksTech) {
+      const lengthTask = input["tasks"][task]
+      res.push([task, lengthTask])
+    }
+    return res
+  }
+
+  const currentTech = "Tec1"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        Depachar isto :)
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/ANI2" element={<LandPage input={getInput()}/>}></Route>
+            <Route path="/ANI2/b" element={<TechMain name={currentTech} listTasks={getTasksTech(currentTech)} />} />
+                      <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
