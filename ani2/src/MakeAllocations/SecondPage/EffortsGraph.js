30205da7b035677753dae2a90672dca5ac1b6575
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2024-04-04 02:32:21 +0100

    Graph now shows and not shows total efforts

--- ani2/src/MakeAllocations/SecondPage/EffortsGraph.js
+++ ani2/src/MakeAllocations/SecondPage/EffortsGraph.js
@@ -8,15 +8,45 @@ import Button from '@mui/material/Button';
 
 export default function EffortsGraph({ current_efforts, allocations, costsProjs, totalWorkHours }) {
 
+  const labels = Object.keys(current_efforts).sort()
+  
+  const current_efforts_tecns = labels.map((tecnId) => current_efforts[tecnId])
+
+  const getEffortsCurrentAllcoation = () => {
+    const currsEffort = []
+    for (const tecnId of labels) {
+      if (allocations[tecnId] !== undefined) {
+        let costThisTecn = 0
+        for (const projId of allocations[tecnId]) {
+          costThisTecn += parseInt(costsProjs[projId]["Esforço"])
+        }
+        currsEffort.push(costThisTecn)
+      }
+      else {
+        currsEffort.push(0)
+      }
+    }
+    return currsEffort
+  }
+  // Used to calculate if option of select max is showed or not
+  const allocationEfforts1 = getEffortsCurrentAllcoation()
+  const totalEfforts1 = current_efforts_tecns.map(function (num, idx) {
+    return num + allocationEfforts1[idx];
+  })
+  const indexOfLargestValue1 = totalEfforts1.reduce((maxIndex, currentValue, currentIndex, array) => currentValue > array[maxIndex] ? currentIndex : maxIndex, 0);
+  const maxEffort = parseInt(totalEfforts1[indexOfLargestValue1])
+  
+  const showOptionChangeTotalEffort = totalWorkHours > maxEffort * 1.1
+  
   const [totalWork, setTotalWork] = useState(true);
+  
   const options = {
     plugins: {
       title: {
         display: true,
         text: "Esforços totais por técnico",
       },
-  },
-    responsive: true,
+    },
     scales: {
       x: [{
         ticks: {
@@ -38,7 +68,7 @@ export default function EffortsGraph({ current_efforts, allocations, costsProjs,
           display: true,
           text: "Horas de trabalho"
         },
-          max: totalWork ? Math.floor(totalWorkHours*1.1) : undefined,
+        max: totalWork ? Math.floor(totalWorkHours * 1.1) : undefined,
       },
     },
   };
@@ -50,62 +80,45 @@ export default function EffortsGraph({ current_efforts, allocations, costsProjs,
 
 
   //const labels = ['Técnico 0', 'Técnico 1', 'Técnico 2', 'Técnico 3', 'Técnico 4', 'Técnico 5', 'Técnico 6', 'Técnico 7', 'Técnico 8', 'Técnico 9', 'Técnico 10', 'Técnico 11', 'Técnico 12', 'Técnico 13', 'Técnico 14', 'Técnico 15', 'Técnico 16', 'Técnico 17', 'Técnico 18', 'Técnico 19', 'Técnico 20', 'Técnico 21', 'Técnico 22', 'Técnico 23', 'Técnico 24', 'Técnico 25', 'Técnico 26', 'Técnico 27', 'Técnico 28', 'Técnico 29', 'Técnico 30', 'Técnico 31', 'Técnico 32']
-  const labels = Object.keys(current_efforts).sort()
-  const getEffortsCurrentAllcoation = () => {
-    const currsEffort = []
-    for (const tecnId of labels) {
-      if (allocations[tecnId] !== undefined) {
-        let costThisTecn = 0
-        for (const projId of allocations[tecnId]) {
-          costThisTecn += parseInt(costsProjs[projId]["Esforço"])
-        }
-        currsEffort.push(costThisTecn)
-      }
-      else {
-        currsEffort.push(0)
-      }
-    }
-    return currsEffort
-  }
-  const current_efforts_tecns = labels.map((tecnId) => current_efforts[tecnId])
   const datasets1 = [
-      {
-        label: 'Esforços alocados',
-        data: current_efforts_tecns,
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Esforços a alocar',
-        data: getEffortsCurrentAllcoation(),
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-      {
-        label: 'Média',
-        data: [],
-        backgroundColor: 'black',
-      },
+    {
+      label: 'Esforços alocados',
+      data: current_efforts_tecns,
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Esforços a alocar',
+      data: getEffortsCurrentAllcoation(),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+    {
+      label: 'Média',
+      data: [],
+      backgroundColor: 'black',
+    },
 
-      
-      {
-        type: 'line',
-        label: 'Média +/- Desvio padrão',
-        data: [],
-        //borderDash: [30, 10],
-        //backgroundColor: "transparent",
-         //borderColor: "grey",
-        backgroundColor: "grey",
 
-      },
-{
-        label: 'Total de horas',
-        data: [],
-        backgroundColor: 'red',
-      },
-    ]
+    {
+      type: 'line',
+      label: 'Média +/- Desvio padrão',
+      data: [],
+      //borderDash: [30, 10],
+      //backgroundColor: "transparent",
+      //borderColor: "grey",
+      backgroundColor: "grey",
+
+    },
+    {
+      label: 'Total de horas',
+      data: [],
+      backgroundColor: 'red',
+    },
+  ]
 
   const data = {
     labels: labels,
-    datasets: totalWork ? datasets1 : datasets1.slice(0,-1)  };
+    datasets: totalWork ? datasets1 : datasets1.slice(0, -1)
+  };
 
 
 
@@ -132,16 +145,18 @@ export default function EffortsGraph({ current_efforts, allocations, costsProjs,
 
     return {
       'Média': average(totalEfforts), 'Desvio padrão': getStandardDeviation(totalEfforts)
-      , 'Amplitude': maxValue - minValue, 'Esforço Máximo': `${maxValueTecn} (${maxValue})`, 
+      , 'Amplitude': maxValue - minValue, 'Esforço Máximo': `${maxValueTecn} (${maxValue})`,
       'Esforço Mínimo': `${minValueTecn} (${minValue})`,
       'Desvio máximo': `${maxValue - average(totalEfforts)}`,
-      'Desvio mínimo': `${ Math.min(average(totalEfforts) - minValue, 0)}`,
+      'Desvio mínimo': `${Math.min(average(totalEfforts) - minValue, 0)}`,
       'Total de horas': `${totalWorkHours}`
     }
   }
   const metricsCalculated = metrics()
 
-const myLine= {
+
+
+  const myLine = {
     id: 'myLine',
     beforeDatasetsDraw(chart, args, plugin) {
       const { ctx, scales: { x, y }, chartArea: { left, right } } = chart;
@@ -150,7 +165,7 @@ const myLine= {
         ctx.beginPath();
         ctx.strokeStyle = lineColor;
         const valueDotted = dotted ? 6 : 0
-        ctx.setLineDash([valueDotted,valueDotted])
+        ctx.setLineDash([valueDotted, valueDotted])
         ctx.lineWidth = 1;
         ctx.moveTo(left, y.getPixelForValue(yCoor))
         ctx.lineTo(right, y.getPixelForValue(yCoor))
@@ -160,7 +175,6 @@ const myLine= {
       drawLine('grey', metricsCalculated["Média"] + metricsCalculated["Desvio padrão"], true)
       drawLine('grey', Math.max(0, metricsCalculated["Média"] - metricsCalculated["Desvio padrão"]), true)
 
-      console.log("NOT Paint red")
     }
   }
   const myLineRed = {
@@ -172,19 +186,19 @@ const myLine= {
         ctx.beginPath();
         ctx.strokeStyle = lineColor;
         const valueDotted = dotted ? 6 : 0
-        ctx.setLineDash([valueDotted,valueDotted])
+        ctx.setLineDash([valueDotted, valueDotted])
         ctx.lineWidth = 1;
         ctx.moveTo(left, y.getPixelForValue(yCoor))
         ctx.lineTo(right, y.getPixelForValue(yCoor))
         ctx.stroke()
+
       }
       drawLine('black', metricsCalculated["Média"], false)
       drawLine('grey', metricsCalculated["Média"] + metricsCalculated["Desvio padrão"], true)
       drawLine('grey', Math.max(0, metricsCalculated["Média"] - metricsCalculated["Desvio padrão"]), true)
 
-      if (totalWork) drawLine('red', totalWorkHours, false)
-      if (totalWork) console.log(`Paint red ${totalWork} |`)
-      
+      drawLine('red', totalWorkHours, false)
+
     }
   }
   const button = (onClicki, text) => {
@@ -200,31 +214,30 @@ const myLine= {
 
     )
   }
-  console.log("Total work?")
-  console.log(totalWork)
   return (
     <div className='horizontalFlex1' >
-      {totalWork ? 
-       <Bar
-        className='growBarChart'
-        options={options}
-        data={data}
-        plugins={[myLineRed]}
-      />
-      :
-<Bar
-        className='growBarChart'
-        options={options}
-        data={data}
-        plugins={[myLine]}
-      />
+
+      {totalWork ?
+        <Bar
+          className='growBarChart'
+          options={options}
+          data={data}
+          plugins={[myLineRed]}
+        />
+        :
+        <Bar
+          className='growBarChart'
+          options={options}
+          data={data}
+          plugins={[myLine]}
+        />
       }
-     
+
       <div className='verticalFlex'>
-      <div className='statsTable'>
-        <StatsTable input={metrics()}  />
-</div>
-{button(switchShowTotalWorkHours, "Visualizar esf. total")}
+        <div className='statsTable'>
+          <StatsTable input={metrics()} />
+        </div>
+        {showOptionChangeTotalEffort && button(switchShowTotalWorkHours, "Visualizar esf. total")}
       </div>
     </div>
   )
